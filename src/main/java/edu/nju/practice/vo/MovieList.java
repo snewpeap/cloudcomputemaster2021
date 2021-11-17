4a03afaa6a080e43468dc69a64711f66b9499394
乐 丁 <mf21320030@smail.nju.edu.cn>
2021-11-17 20:32:40 +0800

    Update src/main/java/edu/nju/practice/vo/MovieList.java

---
+++ src/main/java/edu/nju/practice/vo/MovieList.java
@@ -0,0 +1,45 @@
+package edu.nju.practice.vo;
+
+import java.util.List;
+
+public class MovieList 
+{
+	private List<Movie> genreMovies;
+	private List<Movie> countryMovies;
+	private List<Movie> cityMovies;
+	
+	public MovieList() {
+		super();
+	}
+
+	public MovieList(List<Movie> genreMovies, List<Movie> countryMovies, List<Movie> cityMovies) {
+		super();
+		this.genreMovies = genreMovies;
+		this.countryMovies = countryMovies;
+		this.cityMovies = cityMovies;
+	}
+	
+	public List<Movie> getGenreMovies() {
+		return genreMovies;
+	}
+	public void setGenreMovies(List<Movie> genreMovies) {
+		this.genreMovies = genreMovies;
+	}
+	public List<Movie> getCountryMovies() {
+		return countryMovies;
+	}
+	public void setCountryMovies(List<Movie> countryMovies) {
+		this.countryMovies = countryMovies;
+	}
+	public List<Movie> getCityMovies() {
+		return cityMovies;
+	}
+	public void setCityMovies(List<Movie> cityMovies) {
+		this.cityMovies = cityMovies;
+	}
+
+	public String toString() {
+		return "MovieMap [genreMovies=" + genreMovies + ", countryMovies=" + countryMovies + ", cityMovies="
+				+ cityMovies + "]";
+	}
+}
