c5bcf54479df2ec3401dd0a57c448784c848f3a7
乐 丁 <mf21320030@smail.nju.edu.cn>
2021-11-18 00:50:21 +0800

    Update src/main/java/edu/nju/practice/util/HdfsComponent.java

--- src/main/java/edu/nju/practice/util/HdfsComponent.java
+++ src/main/java/edu/nju/practice/util/HdfsComponent.java
@@ -1,16 +1,32 @@
 package edu.nju.practice.util;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HdfsComponent 
 {
+	@Value("${hdfs.directory}")
+	private String directory;
+	
+	@Value("${hadoop.conf.dir}")
+	private String dir;
+	
 	@Autowired
 	private SocketUtil socketUtil;
 	
 	public int modifyFile()
 	{
-		return socketUtil.startSocket();
+		try {
+			new HdfsUtil(dir).modifyTime(directory);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return 1;
+		//return socketUtil.startSocket();
 	}
 }
