e3eefc221cf969f9d602fc7e4e50b22fa1650e00
乐 丁 <mf21320030@smail.nju.edu.cn>
2021-11-17 20:31:29 +0800

    Update src/main/java/edu/nju/practice/util/SparkUtil.java

--- src/main/java/edu/nju/practice/util/SparkUtil.java
+++ src/main/java/edu/nju/practice/util/SparkUtil.java
@@ -1,10 +1,11 @@
 package edu.nju.practice.util;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.streaming.Durations;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,10 @@ import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
 
+import edu.nju.practice.config.SparkConfig;
 import edu.nju.practice.vo.Movie;
+import edu.nju.practice.vo.MovieList;
+import scala.Tuple2;
 
 @Component
 public class SparkUtil 
@@ -21,38 +25,30 @@ public class SparkUtil
 	@Value("${socket.port}")
 	private int port;
 	
-	@Value("${socket.second}")
-	private int second;
+	@Value("${hdfs.directory}")
+	private String directory;
+	
+	@Value("${spark.timeoutSecond}")
+	private int timeoutSecond;
 	
 	@Autowired
 	private QueueUtil queueUtil;
 	
+	@Autowired
+	private SparkConfig sparkConfig;
+	
 	private int count;
 	
-	private static final String hostname="localhost";
+	private JavaStreamingContext javaStreamingContext;
 	
-	public SparkSession getSession()
-	{
-		SparkSession sparkSession = SparkSession
-			.builder()
-			.appName("SparkUtil")
-			.master("local[*]")
-			.getOrCreate();
-		
-		sparkSession.sparkContext().setLogLevel("WARN");
-		
-		return sparkSession;
-	}
+	private static final String hostname="localhost";
 	
 	public void startStreaming()
 	{
 		count=0;
 		
 		new Thread(()->{
-			SparkSession sparkSession=this.getSession();
-			JavaSparkContext javaSparkContext=new JavaSparkContext(sparkSession.sparkContext());
-			JavaStreamingContext javaStreamingContext=new JavaStreamingContext(javaSparkContext, 
-				Durations.seconds(second));
+			this.javaStreamingContext=sparkConfig.getStreamingContext();
 			
 			JavaReceiverInputDStream<String> lines=javaStreamingContext
 				.socketTextStream(hostname, port);
@@ -63,21 +59,128 @@ public class SparkUtil
 				System.out.println(javaRDD.collect());
 				queueUtil.push(movies);
 				
-				// 10次movies为空，则停止Streaming上下文
+				// 超时
 				if(movies.size()==0)
-				{
-					count++;
-					if(count>10)
-						javaStreamingContext.stop(false);
-				}
+					this.checkTimeout();
 			});
-					
-			javaStreamingContext.start();
-			try {
-				javaStreamingContext.awaitTermination();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			
+			// 启动StreamingContext
+			this.startStreamingContext();
 		}).start();
+	}
+	
+	public void startMonitorHdfs()
+	{
+		count=0;
+		
+		new Thread(()->{
+			this.javaStreamingContext=sparkConfig.getStreamingContext();
+			
+			//JavaDStream<String> lines=javaStreamingContext.textFileStream(directory);
+			JavaDStream<String> lines=javaStreamingContext.socketTextStream(hostname, port);
+			lines.print();
+			
+			lines.foreachRDD(javaRDD->{
+				// 电影类型
+				List<Movie> genreMovies=this.computeByGenre(javaRDD);
+				// 出品国家
+				List<Movie> countryMovies=this.computeByCountry(javaRDD);
+				// 观影地区
+				List<Movie> cityMovies=this.computeByCity(javaRDD);
+				
+				queueUtil.pushList(new MovieList(genreMovies, countryMovies, cityMovies));
+					
+				System.out.println(cityMovies);
+
+				// 超时
+				if(genreMovies.size()==0)
+					this.checkTimeout();
+			});
+			
+			// 启动StreamingContext
+			this.startStreamingContext();
+		}).start();
+	}
+	
+	private List<Movie> computeByGenre(JavaRDD<String> javaRDD)
+	{
+		List<Movie> movies=javaRDD.map(line->new Gson().fromJson(line, Movie.class))
+			.flatMap(movie->{
+				List<Movie> movieList=movie.getGenre().stream()
+					.map(genre->{
+						Movie newMovie=new Movie(movie);
+						newMovie.setGenre(Arrays.asList(genre));
+						return newMovie;
+					}).collect(Collectors.toList());
+				return movieList.iterator();
+			}).mapToPair(movie->new Tuple2<>(movie.getGenre().get(0), movie))
+			.reduceByKey((movie1, movie2)->{
+				Movie movie=new Movie();
+				movie.setDate(movie1.getDate());
+				movie.setGenre(Arrays.asList(movie1.getGenre().get(0)));
+				movie.setAudience(movie1.getAudience()+movie2.getAudience());
+				return movie;
+			}).map(tuple->tuple._2())
+			.collect();
+		
+		return movies;
+	}
+	
+	private List<Movie> computeByCountry(JavaRDD<String> javaRDD)
+	{
+		List<Movie> movies=javaRDD.map(line->new Gson().fromJson(line, Movie.class))
+			.flatMap(movie->{
+				List<Movie> movieList=movie.getCountry().stream()
+					.map(country->{
+						Movie newMovie=new Movie(movie);
+						newMovie.setCountry(Arrays.asList(country));
+						return newMovie;
+					}).collect(Collectors.toList());
+				return movieList.iterator();
+			}).mapToPair(movie->new Tuple2<>(movie.getCountry().get(0), movie))
+			.reduceByKey((movie1, movie2)->{
+				Movie movie=new Movie();
+				movie.setDate(movie1.getDate());
+				movie.setCountry(Arrays.asList(movie1.getCountry().get(0)));
+				movie.setAudience(movie1.getAudience()+movie2.getAudience());
+				return movie;
+			}).map(tuple->tuple._2())
+			.collect();
+		
+		return movies;
+	}
+	
+	private List<Movie> computeByCity(JavaRDD<String> javaRDD)
+	{
+		List<Movie> movies=javaRDD.map(line->new Gson().fromJson(line, Movie.class))
+			.mapToPair(movie->new Tuple2<>(movie.getCity(), movie))
+			.reduceByKey((movie1, movie2)->{
+				Movie movie=new Movie();
+				movie.setDate(movie1.getDate());
+				movie.setCity(movie1.getCity());
+				movie.setAudience(movie1.getAudience()+movie2.getAudience());
+				return movie;
+			}).map(tuple->tuple._2())
+			.collect();
+		
+		return movies;
+	}
+	
+	private void checkTimeout()
+	{
+		// 数据为空超过timeoutSecond，则停止Streaming上下文
+		count++;
+		if(count>timeoutSecond)
+			javaStreamingContext.stop(false);
+	}
+	
+	private void startStreamingContext()
+	{
+		javaStreamingContext.start();
+		try {
+			javaStreamingContext.awaitTermination();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
