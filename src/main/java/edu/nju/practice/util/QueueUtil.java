9087f187637063c9edfef0b256b944cbade9a27b
乐 丁 <mf21320030@smail.nju.edu.cn>
2021-11-17 20:30:20 +0800

    Update src/main/java/edu/nju/practice/util/QueueUtil.java

--- src/main/java/edu/nju/practice/util/QueueUtil.java
+++ src/main/java/edu/nju/practice/util/QueueUtil.java
@@ -7,19 +7,31 @@ import java.util.Queue;
 import org.springframework.stereotype.Component;
 
 import edu.nju.practice.vo.Movie;
+import edu.nju.practice.vo.MovieList;
 
 @Component
 public class QueueUtil 
 {
-	private Queue<List<Movie>> queue=new LinkedList<List<Movie>>();
+	private Queue<List<Movie>> movieQueue=new LinkedList<List<Movie>>();
+	private Queue<MovieList> movieListQueue=new LinkedList<MovieList>();
 	
 	public synchronized boolean push(List<Movie> movies)
 	{
-		return queue.offer(movies);
+		return movieQueue.offer(movies);
 	}
 	
 	public synchronized List<Movie> pop()
 	{
-		return queue.poll();
+		return movieQueue.poll();
+	}
+	
+	public synchronized boolean pushList(MovieList movieList)
+	{
+		return movieListQueue.offer(movieList);
+	}
+	
+	public synchronized MovieList popList()
+	{
+		return movieListQueue.poll();
 	}
 }
